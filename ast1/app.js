c7e81e24ee303e38335aedf9999d64562556155e
ZisanAalam <47499198+ZisanAalam@users.noreply.github.com>
2021-12-26 17:05:18 +0545

    slidding events added

--- ast1/app.js
+++ ast1/app.js
@@ -17,64 +17,93 @@ for (let i = 0; i < imgCount; i++) {
     img.style.left = `${i*imgWidth}px`;
 }
 
-
+let currentIndex = 0;
+let targetIndex = 0;
 
 //Initialization of Previous
 var prev = document.createElement("div");
 prev.setAttribute("class", "prev");
-prev.textContent = '<';
+prev.innerHTML = '&#60;';
 carouselWrapper.appendChild(prev);
 
+//Attaching Click Event to Previous button
+prev.onclick = function() {
+    if (currentIndex == 0) currentIndex = imgCount;
+    // currentIndex--;
+    targetIndex = currentIndex - 1;
+    animateTransition(targetIndex);
+    currentIndex = targetIndex;
 
+}
+
+// Initialization of next button
 var next = document.createElement("div");
 next.setAttribute("class", "next");
-next.textContent = '>';
+next.innerHTML = '&#62;';
 carouselWrapper.appendChild(next);
 
 
-let currentIndex = 0;
-// let timer;
-// let dx = 0;
-// let target;
+//Attaching click event to Next Button
 next.onclick = function() {
-    currentIndex++;
-
-    if (currentIndex == imgCount) {
-        currentIndex = 0;
-        // dx = 0;
+    targetIndex = currentIndex + 1;
+    if (targetIndex == imgCount) {
+        targetIndex = 0;
     }
-    // target = `${currentIndex*imgWidth}`;
-    // timer = setInterval(() => {
-    //     dx++;
-    //     if (dx >= target) {
-    //         clearInterval(timer);
-    //     } else {
-    //         carouselImages.style.left = `-${dx}px`
-    //     }
 
+    animateTransition(targetIndex);
+    currentIndex = targetIndex;
 
-    // }, 1);
-
-    console.log(currentIndex);
-
-    carouselImages.style.left = `-${currentIndex*imgWidth}px`;
 }
 
 
 
 
-//Attaching Click Event to Next button
-prev.onclick = function() {
-    if (currentIndex == 0) currentIndex = imgCount;
-    currentIndex--;
-    // target = `${target-(currentIndex*imgWidth)}`;
-    carouselImages.style.left = `-${currentIndex*imgWidth}px`;
-    // console.log(currentIndex);
+
+
+
+//Dot Navigation
+
+let dotContainer = document.createElement('div');
+dotContainer.setAttribute('class', 'dots');
+var dotArr = [];
+for (let i = 0; i < imgCount; i++) {
+    var dot = document.createElement('span');
+    dot.setAttribute('class', 'dot');
+    dot.innerHTML = ". &nbsp;";
+    dot.onclick = function() {
+        animateTransition(i);
+    }
+
+
+    dotArr.push(dot);
+    dotContainer.appendChild(dot);
+
+}
+carouselWrapper.appendChild(dotContainer);
+
+
+let targetLetf;
+let dx = 0;
+let timer;
+
+var getStyle = function(element, style) {
+    return parseFloat(window.getComputedStyle(element).getPropertyValue(style));
+};
+
+function animateTransition(index) {
+    // currentLeft = getStyle(carouselImages.children[index], 'left');
+    // console.log(currentLeft);
+    // targetLetf = index * imgWidth;
+    carouselImages.style.left = `-${index*imgWidth}px`;
+    // console.log(targetLetf);
     // timer = setInterval(() => {
-    //                 dx--;
-    //                 carouselImages.style.left = `-${dx}px`
-    //                 if (dx >= target) {
-    //                     clearInterval(timer);
-    //                 }
-    //             }, 1);
+
+    //     if (dx >= targetLetf + 1) {
+    //         clearInterval(timer);
+    //         dx = currentLeft;
+    //     }
+    //     carouselImages.style.left = `-${dx}px`;
+    //     dx += 10;
+    // }, 1);
+
 }
