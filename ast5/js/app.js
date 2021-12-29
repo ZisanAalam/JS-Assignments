c6f15fa4e181590e82c36814e9061cabdeea7c93
ZisanAalam <47499198+ZisanAalam@users.noreply.github.com>
2021-12-29 14:53:51 +0545

    flapping effect added

--- ast5/js/app.js
+++ ast5/js/app.js
@@ -1,50 +1,37 @@
-// const gameArea = document.querySelector('.gameArea');
-
-let bird = document.querySelector('.bird');
+let bird = document.querySelector(".bird");
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min) + min);
 }
-console.log(window.getComputedStyle(bird).getPropertyPriority("tops"))
-document.addEventListener('keydown', event => {
-    if (event.code === 'Space') {
-        console.log('Space pressed'); //whatever you want to do when space is pressed
-        // console.log(window.getComputedStyle(bird).getPropertyPriority("tops"));
-        // console.log(bird.getBoundingClientRect().y);
-        // // bird.style.top = parseInt(`${bird.getBoundingClientRect().y}`) - 1 + 'px';
-        // bird.style.top -= bird.getBoundingClientRect().y - 1 + 'px';
-        // // bird.style.top = (350 - 0.1) + 'px';
-
-        // console.log(birdO bj);
-        // 
-
-        console.log(bird);
+document.addEventListener("keydown", (event) => {
+    if (event.code === "Space") {
+        event.preventDefault();
+        bird.style.top = parseInt(`${bird.style.top}`) - 15 + "px";
     }
-})
+});
 
-
-document.addEventListener('keyup', event => {
-    if (event.code === 'Space') {
-        console.log('Space released'); //whatever you want to do when space is pressed
+document.addEventListener("keyup", (event) => {
+    if (event.code === "Space") {
+        event.preventDefault();
     }
-})
+});
 
 let index = 1;
 class Bird {
-    draw() {
-        bird.style.backgroundImage = `url(images/bird${ index }.png)`;
-        index = (index + 1) % 3 + 1;
-        // bird.style.top = 250 + 'px';
+    constructor() {
+        bird.style.top = 250 + "px";
+    }
+    flap() {
+        bird.style.backgroundImage = `url(images/bird${index}.png)`;
+        index = ((index + 1) % 3) + 1;
     }
 
-    update() {
-
-    }
+    update() {}
 }
 
 let birdObj = new Bird();
 timer = setInterval(() => {
-    birdObj.draw();
-}, 100)
+    birdObj.flap();
+}, 100);
